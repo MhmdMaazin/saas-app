4b07a8826b4f01c7bb0a5d4d7567d88d774c51e8
MhmdMaazin <mazimaazin@gmail.com>
2025-05-26 10:44:27 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,52 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Welcome to the Page</h1>
-      <Button>
-        Click Me
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard 
+          id="1"
+          name="Sample Name"
+          topic="Sample Topic"
+          subject="Sample Subject"
+          duration={60}
+          color="#ffda6e"
+          bookmarked={false}
+        />
+        <CompanionCard 
+          id="2"
+          name="Another Companion"
+          topic="Another Topic"
+          subject="Another Subject"
+          duration={45}
+          color="#e5d0ff"
+          bookmarked={true}
+          />
+        <CompanionCard
+          id="3"
+          name="Third Companion"
+          topic="Third Topic"
+          subject="Third Subject"
+          duration={30}
+          color="#bde7ff"
+          bookmarked={false}
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full" 
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
